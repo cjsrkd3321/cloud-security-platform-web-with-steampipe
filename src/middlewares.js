4a5504d25f4f56925647834b6568b12bcd98da09
cjsrkd3321 <7424069@gmail.com>
2022-01-01 21:27:45 +0900

    Remove console.log()

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
-  console.log(res.locals);
   next();
 };
 
