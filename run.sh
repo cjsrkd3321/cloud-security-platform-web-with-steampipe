4a224c84f3e1870f4cdbe3e40e189429c471d7cd
cjsrkd3321 <7424069@gmail.com>
2022-01-02 12:58:29 +0900

    Add steampipe image in docker-compose.yml

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ echo DB_URL=mongodb://mongodb/cspw2s >> .env
 
 docker-compose build --no-cache
 docker-compose up -d
+docker exec -it steampipe steampipe plugin install steampipe && docker exec -it steampipe steampipe plugin install aws
