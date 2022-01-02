b8b712ebbdefb77f73b37afa8c1ea74d32434967
cjsrkd3321 <7424069@gmail.com>
2022-01-02 14:29:32 +0900

    Update db.js for pg connection

--- src/db.js
+++ src/db.js
@@ -1,8 +1,27 @@
 import mongoose from 'mongoose';
+import { Client } from 'pg';
 
+// MongoDB
 mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 
-db.on('error', (err) => console.log('DB Error', err));
-db.once('open', () => console.log('🚀 Connected to DB'));
+db.on('error', (err) => console.error('[MongoDB] DB Error', err));
+db.once('open', () => console.log('[MongoDB] 🚀 Connected to DB'));
+
+// PostgreSQL
+const pg = new Client({
+  user: 'steampipe',
+  host: process.env.SP_HOST,
+  database: 'steampipe',
+  password: process.env.SP_PASSWORD,
+  port: 9193,
+});
+
+pg.connect((err) => {
+  if (err) {
+    console.error('[PostgreSQL] DB Error', err);
+  } else {
+    console.log('[PostgreSQL] 🚀 Connected to DB');
+  }
+});
