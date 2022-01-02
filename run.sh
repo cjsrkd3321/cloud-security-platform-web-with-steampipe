3be35aed2d692e176addbe0f7ee7a472f65195a0
cjsrkd3321 <7424069@gmail.com>
2022-01-02 14:29:45 +0900

    update docker-compose

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 echo COOKIE_SECRET=$(uuidgen) > .env
 echo DB_URL=mongodb://mongodb/cspw2s >> .env
+echo SP_HOST=steampipe >> .env
+echo SP_PASSWORD=$(uuidgen) >> .env
 
 docker-compose build --no-cache
 docker-compose up -d
+
 docker exec -it steampipe steampipe plugin install steampipe && docker exec -it steampipe steampipe plugin install aws
+
