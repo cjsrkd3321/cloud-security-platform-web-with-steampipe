42ea7e018d192b1d8322061ffcd3f4ed534aa729
cjsrkd3321 <7424069@gmail.com>
2022-01-03 23:06:08 +0900

    Fix query timeout for relation does not exist

--- src/controllers/awsController.js
+++ src/controllers/awsController.js
@@ -1,8 +1,17 @@
 import { pg } from '../db';
 
 export const awsHome = async (req, res) => {
-  const testData = await pg.query('SELECT * FROM aws_account');
-  const rows = testData.rows;
+  const pageTitle = 'Cloud-Home';
+  let rows = [];
+  try {
+    const testData = await pg.query('SELECT * FROM aws_account');
+    rows = testData.rows;
+  } catch (err) {
+    return res.status(400).render('join', {
+      pageTitle,
+      errorMessage: `[aws][awsHome] ${err} / please retry.`,
+    });
+  }
 
-  res.render('cloud-home', { pageTitle: 'Cloud-Home', rows });
+  return res.render('cloud-home', { pageTitle, rows });
 };
--- src/db.js
+++ src/db.js
@@ -16,6 +16,7 @@ export const pg = new Client({
   database: 'steampipe',
   password: process.env.SP_PASSWORD,
   port: 9193,
+  query_timeout: 1000 * 60, // 1 min
 });
 
 pg.connect((err) => {
