819b040774af8e718729e6a1bec8e194f92819b3
cjsrkd3321 <7424069@gmail.com>
2022-01-05 18:47:48 +0900

    Delete inline_policy

--- terraform/ec2-role.tf
+++ terraform/ec2-role.tf
@@ -13,19 +13,4 @@ resource "aws_iam_role" "ec2-role" {
   name                = "cspw2s_role"
   assume_role_policy  = data.aws_iam_policy_document.ec2-assume-role-policy.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/ReadOnlyAccess", "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
-
-  inline_policy {
-    name = "GenerateCredentialReportPolicy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["iam:GenerateCredentialReport"]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
-  }
 }
