afd94edbb7e7fee777f548c52de0508634ba3ecd
cjsrkd3321 <7424069@gmail.com>
2022-01-08 22:37:02 +0900

    Delete no use env variable

--- dev-run.sh
+++ dev-run.sh
@@ -1,7 +1,6 @@
 echo COOKIE_SECRET=$(uuidgen) > .env
 echo DB_URL=mongodb://localhost/cspw2s >> .env
 echo SP_HOST=localhost >> .env
-echo SP_PASSWORD=$(uuidgen) >> .env
 echo SP_TTL=300 >> .env
 
 npm install
