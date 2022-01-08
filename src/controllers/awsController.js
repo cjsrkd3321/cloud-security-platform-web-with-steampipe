7be7d2deb3fe11d2c6b8de07f079685b137127c1
cjsrkd3321 <7424069@gmail.com>
2022-01-08 22:10:46 +0900

    Update dynamic query table

--- src/controllers/awsController.js
+++ src/controllers/awsController.js
@@ -1,19 +1,32 @@
 import { pg } from '../db';
-import { awsAccount } from '../queries/aws';
+import * as awsQueries from '../queries/aws';
 
 export const awsHome = async (req, res) => {
-  const pageTitle = 'Cloud-Home';
+  const queries = Object.keys(awsQueries);
+  return res.render('cloud-home', { pageTitle: 'Cloud-Home', queries });
+};
+
+export const awsTable = async (req, res) => {
+  const { table } = req.params;
+  const pageTitle = 'Cloud-Table';
   let rows = [];
 
+  const query =
+    awsQueries[Object.keys(awsQueries).find((query) => query === table)];
+  if (!query) {
+    return res.status(400).redirect('/aws');
+  }
+
   try {
-    const testData = await pg.query(awsAccount);
-    rows = testData.rows;
+    const queryResult = await pg.query(query);
+    rows = queryResult.rows;
   } catch (err) {
-    return res.status(400).render('cloud-home', {
+    return res.status(400).render('cloud-table', {
       pageTitle,
-      errorMessage: `[aws][awsHome] ${err}`,
+      rows: [],
+      errorMessage: `[aws][awsTable] ${err}`,
     });
   }
 
-  return res.render('cloud-home', { pageTitle, rows });
+  return res.render('cloud-table', { pageTitle, rows });
 };
--- src/routers/awsRouter.js
+++ src/routers/awsRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { awsHome } from '../controllers/awsController';
+import { awsHome, awsTable } from '../controllers/awsController';
 import { protectMiddleware } from '../middlewares';
 
 const awsRouter = express.Router();
 
 awsRouter.route('/').all(protectMiddleware).get(awsHome);
+awsRouter.route('/:table([a-zA-Z]{5,30})').all(protectMiddleware).get(awsTable);
 
 export default awsRouter;
