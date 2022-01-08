d574cf9bc997f6517a6839550b3f9fc164eae64f
cjsrkd3321 <7424069@gmail.com>
2022-01-08 22:20:11 +0900

    Update awsBatch.js

--- src/batch/awsBatch.js
+++ src/batch/awsBatch.js
@@ -1,9 +1,4 @@
 import Batch from '../libs/batch';
-import { awsAccount, awsIamPolicy, awsIamRole } from '../queries/aws';
+import * as awsQueries from '../queries/aws';
 
-// Account
-new Batch(awsAccount);
-
-// IAM
-new Batch(awsIamPolicy);
-new Batch(awsIamRole);
+Object.keys(awsQueries).forEach((query) => new Batch(awsQueries[query]));
