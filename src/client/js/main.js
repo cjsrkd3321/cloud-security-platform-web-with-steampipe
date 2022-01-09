2b2a43460eb0ed79423b4fa435162e19a8a42086
cjsrkd3321 <7424069@gmail.com>
2022-01-09 11:06:44 +0900

    Add search function to client

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,17 @@
 import '../scss/styles.scss';
+
+const search = document.querySelector('input.search');
+
+if (search) {
+  const tables = document.querySelectorAll('a.table');
+
+  search.oninput = (e) => {
+    const data = Array.from(tables).filter((table) => {
+      if (!table.innerText.includes(e.target.value.toUpperCase())) {
+        table.style.display = 'none';
+      } else {
+        table.style.display = '';
+      }
+    });
+  };
+}
