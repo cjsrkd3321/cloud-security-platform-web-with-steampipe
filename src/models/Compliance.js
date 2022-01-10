f1ac84bbe4e3f2c198e2e4168e28812b7a172036
cjsrkd3321 <7424069@gmail.com>
2022-01-10 23:39:51 +0900

    Add compliance model for result saved

---
+++ src/models/Compliance.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const complianceSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlength: 30,
+  },
+  results: [{ type: Map }],
+  createdAt: { type: Date, required: true },
+});
+
+const Compliance = mongoose.model('Compliance', complianceSchema);
+export default Compliance;
