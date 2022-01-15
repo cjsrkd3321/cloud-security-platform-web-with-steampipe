6853ad3abd4b76dc92fb40cbb6bfd2ca8f0b4c6a
cjsrkd3321 <7424069@gmail.com>
2022-01-15 19:09:13 +0900

    Update batch way

--- src/batch/awsBatch.js
+++ src/batch/awsBatch.js
@@ -1,4 +1,6 @@
 import Batch from '../libs/batch';
 import * as awsQueries from '../queries/aws';
 
-new Batch('aws', awsQueries);
+Object.entries(awsQueries).forEach(([title, query]) =>
+  Batch.batchQuery(title, query)
+);
--- src/libs/batch.js
+++ src/libs/batch.js
@@ -1,19 +1,9 @@
 import { pg } from '../db';
-import { DEFAULT_TTL, SECOND } from '../libs/time';
+import { DEFAULT_TTL } from '../libs/time';
 import Compliance from '../models/Compliance';
-import { isPluginExists } from '../queries/common';
 
 class Batch {
-  constructor(cloud, queries) {
-    Batch.#cloud = cloud;
-    Batch.#queries = queries;
-    Batch.runBatch();
-  }
-
-  static #cloud;
-  static #queries;
-
-  static async querySteampipe(title, query) {
+  static async batchQuery(title, query) {
     try {
       const results = (await pg.query(query)).rows;
 
@@ -29,24 +19,7 @@ class Batch {
     } catch (err) {
       console.error(err);
     } finally {
-      setTimeout(Batch.querySteampipe, DEFAULT_TTL, title, query);
-    }
-  }
-
-  static getSchema(cloud) {
-    return pg.query(isPluginExists(cloud));
-  }
-
-  static async runBatch() {
-    const result = await Batch.getSchema(Batch.#cloud);
-
-    // If plugin installed
-    if (result.rows[0]) {
-      Object.keys(Batch.#queries).forEach((query) =>
-        Batch.querySteampipe(query, Batch.#queries[query])
-      );
-    } else {
-      setTimeout(Batch.runBatch, 5 * SECOND);
+      setTimeout(Batch.batchQuery, DEFAULT_TTL, title, query);
     }
   }
 }
